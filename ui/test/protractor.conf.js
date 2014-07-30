e492d7cf979f9e9532be12bdcc61f2c0f0d70875
Elian ORIOU <elian.oriou@4sh.fr>
2014-07-31 00:09:33 +0200

    Update protractor conf

--- ui/test/protractor.conf.js
+++ ui/test/protractor.conf.js
@@ -1,10 +1,10 @@
 exports.config = {
 
-    // The address of a running selenium server.
+    // The address of a running selenium server
     seleniumAddress: 'http://localhost:4444/wd/hub',
 
     // App BaseURL
-    baseUrl: 'http://localhost:8080/#/',
+    baseUrl: 'http://localhost:8080/#',
 
     // Capabilities to be passed to the webdriver instance.
     capabilities: {
